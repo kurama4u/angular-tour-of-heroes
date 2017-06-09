2b49a4e4e8822b442a35302dade1aa2430206950
kuramalicious <jv_kurama@yahoo.com>
2017-06-09 20:55:56 +0800

    angular heroes

---
+++ tour-of-heroes/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroComponent } from './hero/hero.component';
+import { MockHeroesComponent } from './mock-heroes/mock-heroes.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard/dashboard.component'
+import { HeroesComponent } from './heroes.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module'
+  
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule,
+    AppRoutingModule 
+  ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroComponent,
+    MockHeroesComponent,
+    DashboardComponent,
+    HeroesComponent,
+  ],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
